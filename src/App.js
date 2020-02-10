aab6b73f91ab46c3001329b465638e105515311d
Katie Wokasch <katie.wokasch@gmail.com>
2020-02-09 17:52:51 -0700

    change form to functional component

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ export default class App extends Component {
     event.preventDefault()
 
     const domainNames = this.state.currentDomainNames.split(', ')
-    
+
     this.setState({ domainNames })
     this.postNames({ domainNames })
   }
